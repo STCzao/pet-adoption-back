0d964e2a8ec545fe656b9a77b965befffb3abbd1
STCzao <Santicoronel0183@gmail.com>
2025-09-20 17:09:11 -0300

    Agregamos el buscador para diferentes tipos de caracteristicas

--- models/server.js
+++ models/server.js
@@ -1,54 +1,56 @@
-const express = require('express');
-const cors = require('cors');
-const { dbConnection } = require('../database/config');
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../database/config");
 
 class Server {
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-        this.paths = {
-            auth: '/api/auth',
-            usuarios: '/api/usuarios',
-            publicaciones: '/api/publicaciones'
-        }
+    this.paths = {
+      auth: "/api/auth",
+      usuarios: "/api/usuarios",
+      publicaciones: "/api/publicaciones",
+      buscar: "/api/buscar",
+    };
 
-        // Conectar a base de datos
-        this.conectarDB();
+    // Conectar a base de datos
+    this.conectarDB();
 
-        // Middlewares
-        this.middlewares();
+    // Middlewares
+    this.middlewares();
 
-        // Rutas de mi aplicación
-        this.routes();
-    }
+    // Rutas de mi aplicación
+    this.routes();
+  }
 
-    async conectarDB() {
-        await dbConnection();
-    }
+  async conectarDB() {
+    await dbConnection();
+  }
 
-    middlewares() {
-        // CORS
-        this.app.use(cors());
+  middlewares() {
+    // CORS
+    this.app.use(cors());
 
-        // Lectura y parseo del body
-        this.app.use(express.json());
+    // Lectura y parseo del body
+    this.app.use(express.json());
 
-        // Directorio público
-        this.app.use(express.static('public'));
-    }
+    // Directorio público
+    this.app.use(express.static("public"));
+  }
 
-    routes() {
-        this.app.use(this.paths.auth, require('../routes/auth'));
-        this.app.use(this.paths.usuarios, require('../routes/usuarios'));
-        this.app.use(this.paths.publicaciones, require('../routes/publicaciones'));
-    }
-
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto:', this.port);
-        });
-    }
+  routes() {
+    this.app.use(this.paths.auth, require("../routes/auth"));
+    this.app.use(this.paths.usuarios, require("../routes/usuarios"));
+    this.app.use(this.paths.publicaciones, require("../routes/publicaciones"));
+    this.app.use(this.paths.buscar, require("../routes/buscar"));
+  }
+  
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto:", this.port);
+    });
+  }
 }
 
 module.exports = Server;
