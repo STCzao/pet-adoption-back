ddef70a97a8b8f185f8e2c962b9eacd160d723f4
STCzao <Santicoronel0183@gmail.com>
2025-11-04 17:15:41 -0300

    Cambio nuevamente en las importaciones

--- routes/casosExito.js
+++ routes/casosExito.js
@@ -5,7 +5,8 @@ const {
   crearCasoExito,
   eliminarCasoExito,
 } = require("../controllers/casosExito");
-const { validarJWT, validarCampos } = require("../middlewares");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
